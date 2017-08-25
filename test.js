c637184c57c97b7d6155dff069ddd8e9f90cb496
1050043110@qq.com <1050043110@qq.com>
2017-08-25 16:50:16 -0400

    Add existing file

--- test.js
+++ test.js
@@ -1,16 +1,51 @@
-var http = require('http'); 
-var port = 80; 
+//Yuzhuo Chen
+//CEN 3031
+//2017-8-23
+
+var http = require('http'), 
+    fs = require('fs'), 
+    url = require('url'),
+    port = 8080;
+
+/* Global variables */
+var listingData, server;
 
 var requestHandler = function(request, response) {
-  response.end('Request received!');
-};
+  var parsedUrl = url.parse(request.url); // convert url to JSON format
 
-// a server is created, but not started
-var server = http.createServer(requestHandler);
+  /*
+    Your request handler should send listingData in the JSON format if a GET request 
+    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
 
-// the server is now started, listening for requests on port 8080
-server.listen(port, function() {
-  //once the server is listening, this callback function is executed
-  console.log('Server listening on: http://127.0.0.1:' + port);
+    HINT: explore the request object and its properties 
+    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+   */
+   if(request.method == 'GET' && parsedUrl.path == '/listings') {
+     response.writeHead(200, {"Content-Type": "application/json"})
+     response.end(JSON.stringify(listingData));
+     //response.end(listingData);
+     response.statusCode = 200;
+   }
+   else {
+     response.writeHead(404);
+     response.end("Bad gateway error");
+     response.statusCode = 400;
+   }
+ };
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  /*
+    This callback function should save the data in the listingData variable, 
+    then start the server. 
+   */
+   if(err) throw err;
+   listingData = JSON.parse(data);   //convert JSON to object
+   //listingData = data;
 });
-console.log('Is the server started?');
+
+server = http.createServer(requestHandler);
+server.listen(port);
+
+//JSON.parse()   convert JSON to object
+//JSON.stringify()  convert objecct to JSON
+//url.parse(request.url);  convert url to JSON format

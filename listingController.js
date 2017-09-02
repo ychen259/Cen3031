0e0e6979a8f9a8e2cb67d519a060df8946cd049d
1050043110@qq.com <1050043110@qq.com>
2017-09-01 21:32:11 -0400

    Assignment 2 v3

---
+++ listingController.js
@@ -0,0 +1,22 @@
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+  function($scope, Listings) {
+    $scope.listings = Listings;
+    $scope.detailedInfo = undefined;
+
+    /* 
+      Implement these functions in the controller to make your application function 
+      as described in the assignment spec. 
+     */
+    $scope.addListing = function() {
+      $scope.listings.push($scope.data);
+      $scope.data = {};
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
+  }
+]);
+
